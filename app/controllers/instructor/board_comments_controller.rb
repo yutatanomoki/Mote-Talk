d46585e36af7a148e52cec1dae0818cb774d3ea0
yutatanomoki <yuta3071@gmail.com>
2021-02-14 10:38:23 +0000

    [add]コメント非同期化

---
+++ app/controllers/instructor/board_comments_controller.rb
@@ -0,0 +1,24 @@
+class Instructor::BoardCommentsController < ApplicationController
+  def create
+    @request_board = RequestBoard.find(params[:request_board_id])
+    @board_comment = BaordComment.new(board_comment_params)
+    @board_comment.book_id = @book.id
+    @board_comment.user_id = current_user.id
+    unless @board_comment.save
+      render 'error'  # app/views/book_comments/error.js.erbを参照する ※要件外
+    end
+    # app/views/board_comments/create.js.erbを参照する
+  end
+
+  def destroy
+    @request_board = RequestBoard.find(params[:request_board_id])
+    board_comment = @request_board.board_comments.find(params[:id])
+    board_comment.destroy
+    # app/views/board_comments/destroy.js.erbを参照する
+  end
+
+  private
+  def board_comment_params
+    params.require(:board_comment).permit(:comment)
+  end
+end
