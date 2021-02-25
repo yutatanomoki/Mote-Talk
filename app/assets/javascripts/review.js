47944874bce09fdfb820b82fe6900288043e027e
yutatanomoki <yuta3071@gmail.com>
2021-02-25 12:04:31 +0000

    [fix] const -> let

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
 	// class = "ratings"の子要素を取得
-	let stars = document.querySelector(".ratings").children;
+	var stars = document.querySelector(".ratings").children;
 	// const stars = document.getElementsByClassName("ratings").children;
 	// id = "rating-value"の要素を取得
-	let ratingValue = document.getElementById("rating-value");
+	var ratingValue = document.getElementById("rating-value");
 	// id = "rating-value-display"の要素を取得
-	let ratingValueDisplay = document.getElementById("rating-value-display");
+	var ratingValueDisplay = document.getElementById("rating-value-display");
 	// indexという変数を定義
-	let index;
+	var index;
 
 	// reveiwの投稿ページに遷移するとstars.lengthの値（5）だけ繰り返し
-	for(let i=0; i<stars.length; i++){
+	for(var i=0; i<stars.length; i++){
 		console.log(stars.length)
 		// 星にカーソルが乗ったときに実行する関数を定義
 		stars[i].addEventListener("mouseover",function(){
 			// stars.lengthの値（5）だけ繰り返し
 			// つまり、星カーソルが乗った時、5回繰り返される
-			for(let j=0; j<stars.length; j++){
+			for(var j=0; j<stars.length; j++){
 				console.log(stars.length)
 				// まず全て星をくり抜く
 				stars[j].classList.remove("fa-star");
 				stars[j].classList.add("fa-star-o");
 			}
 			// そのあと星の数だけ以下の関数が繰り返される。
-			for(let j=0; j<=i; j++){
+			for(var j=0; j<=i; j++){
 				console.log(stars.length)
 				stars[j].classList.remove("fa-star-o");
 				// カーソルが乗った星まで星を塗りつぶす
@@ -41,12 +41,12 @@ $(document).ready(function(){
 		// 星からカーソルが離れたときに実行される関数
 		stars[i].addEventListener("mouseout",function(){
 			// まず5回繰り返す
-			for(let j=0; j<stars.length; j++){
+			for(var j=0; j<stars.length; j++){
 				// まず全ての星をくり抜く
 				stars[j].classList.remove("fa-star");
 				stars[j].classList.add("fa-star-o");
 			}
-			for(let j=0; j<=index; j++){
+			for(var j=0; j<=index; j++){
 				// クリックされている星まで塗りつぶす
 				stars[j].classList.remove("fa-star-o");
 				stars[j].classList.add("fa-star");
