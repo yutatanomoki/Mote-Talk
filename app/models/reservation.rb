547715cb913575f166615eff50e47e05065df066
yutatanomoki <yuta3071@gmail.com>
2021-03-09 12:48:22 +0000

    [fixed]予約時間表示修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :instructor
+  
+  def total_time
+    Time.at(self.finish_time - self.begin_time).strftime("%H:%M")
+  end
 end
+
