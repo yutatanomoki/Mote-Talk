54a627b25b4b877b074424178b54d7a017882528
yutatanomoki <yuta3071@gmail.com>
2021-10-06 09:37:11 +0000

    [fixed]UsersController

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,5 @@
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     @users = User.all
