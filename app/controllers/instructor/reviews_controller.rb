904012795f78643f55a1d4746c22bad1b45786e5
yutatanomoki <yuta3071@gmail.com>
2021-10-07 02:25:16 +0000

    [fixed]プロフィール画像編集

--- app/controllers/instructor/reviews_controller.rb
+++ app/controllers/instructor/reviews_controller.rb
@@ -1,6 +1,3 @@
 class Instructor::ReviewsController < ApplicationController
-  def index
-    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
-    @reviews = @ramen_shop.reviews
-  end
+  
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,7 +7,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @profile_images = @user.profile_images.page(params[:page]).reverse_order
+    @profile_image = @user.profile_image.page(params[:page]).reverse_order
   end
 
   def edit
