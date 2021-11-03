7516b28beb336ad084ff71bf2168873e3b64b14c
yutatanomoki <yuta3071@gmail.com>
2021-11-03 04:14:58 +0000

    [Fixed]favorite-btn

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -10,8 +10,6 @@ class Public::FavoritesController < ApplicationController
   def destroy
     @instructor = Instructor.find(params[:instructor_id])
     favorite = @instructor.favorites.find_by(user_id: current_user.id)
-    
     favorite.destroy
-    binding.pry
   end
 end
